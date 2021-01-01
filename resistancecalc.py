b6ee9555b16afd8ab7cf9af39ff45917bbb8213f
etale0 <76614471+etale0@users.noreply.github.com>
2021-01-01 17:03:36 +0100

    Reformatted code to comply with PEP recommendations. Added main function and __name__=__main__ mechanism.

--- resistancecalc.py
+++ resistancecalc.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Dec 20 20:21:38 2020
+Created on Sun Dec 20 20:21:38 2020.
 
 @author: etale
 """
@@ -10,19 +10,29 @@ from collections import Counter
 import sys
 import re
 
-sys.argv.extend(["-conv=12", "-r=1"])
+# sys.argv.extend(["-conv=0", "-efr=20", "-epr=20"])
 
+# Index of difficulty: name of difficulty
 diff_dic = {0: "Normal", 1: "Nightmare", 2: "Hell"}
 
-res_dic = {0: "Physical", 1:"Magic", 2: "Cold", 
-            3: "Fire", 4: "Lightning", 5: "Poison"}
+# Index of resistance type: name of resistance type
+res_d = {0: "Physical", 1: "Magic", 2: "Cold",
+         3: "Fire", 4: "Lightning", 5: "Poison"}
+
+# "Mod": (ResType, ResBonus)
+mod_dic = {"": (0, 0), "SS": (0, 50), "MB": (1, 20),
+           "CE": (2, 75), "FE": (3, 75), "LE": (4, 75)}
+
 
 class Parameters:
+    """Container for command line options."""
+
     def __init__(self, parms):
         """
-        Initializes the parameter vector which contains the switches/values
-        for conviction, lower resist, cold mastery, -% enemy resist, ranged
-        type as well as difficulty.
+        Initialize the parameter vector.
+
+        Contains the command line options for conviction, lower resist, cold
+        mastery, -% enemy resist, ranged type, difficulty and leaper.
         """
         self.conv = parms[0]
         self.lr = parms[1]
@@ -31,7 +41,9 @@ class Parameters:
         self.rtype = parms[4]
         self.diff = parms[5]
         self.leap = parms[6]
+
     def __str__(self):
+        """List all parameters for convenience."""
         info_str = "*************************\n"
         info_str += "Difficulty: {}\n".format(diff_dic[self.diff])
         info_str += "Ranged Type: {}\n".format("Yes" if self.rtype else "No")
@@ -39,127 +51,145 @@ class Parameters:
         info_str += "Lower Resist Level: {}\n".format(self.lr)
         info_str += "Cold Mastery Level: {}\n".format(self.cm)
         info_str += "-% Enemy Resist: \n"
-        info_str += "\n".join("\t{}: {}".format(res_dic[i], self.er[i]) for i in range(0, 6))
+        enemy_res = ["\t{}: {}".format(res_d[i], self.er[i]) for i in range(6)]
+        info_str += "\n".join(enemy_res)
         info_str += "\n*************************"
         return info_str
 
+
 def read_cmd():
+    """Collect all command line options and put them in a Parameters object."""
     par = Parameters([0, 0, 0, 6*[0], 0, 2, 0])
     for x in sys.argv:
-        #-conv=conviction lvl
+        # -conv=conviction lvl
         if bool(re.fullmatch(r"-conv=\d+", x)):
             par.conv = int(x[6:])
-        #-lr=lower resist lvl
+        # -lr=lower resist lvl
         if bool(re.fullmatch(r"-lr=\d+", x)):
             par.lr = int(x[4:])
-        #-cm=cold mastery lvl
+        # -cm=cold mastery lvl
         if bool(re.fullmatch(r"-cm=\d+", x)):
             par.cm = int(x[4:])
-        #-ecr=-% Enemey Cold Resist
+        # -ecr=-% Enemey Cold Resist
         if bool(re.fullmatch(r"-ecr=\d+", x)):
             par.er[2] = int(x[5:])
-        #-efr=-% Enemey Fire Resist
+        # -efr=-% Enemey Fire Resist
         if bool(re.fullmatch(r"-efr=\d+", x)):
             par.er[3] = int(x[5:])
-        #-elr=-% Enemey Lightning Resist
+        # -elr=-% Enemey Lightning Resist
         if bool(re.fullmatch(r"-elr=\d+", x)):
             par.er[4] = int(x[5:])
-        #-epr=-% Enemey Poison Resist
+        # -epr=-% Enemey Poison Resist
         if bool(re.fullmatch(r"-epr=\d+", x)):
             par.er[5] = int(x[5:])
-        #-r=rangetype
+        # -r=rangetype
         if bool(re.fullmatch(r"-r=[01]", x)):
-            par.rtype = int(x[3])            
-        #-d=difficulty
+            par.rtype = int(x[3])
+        # -d=difficulty
         if bool(re.fullmatch(r"-d=[012]", x)):
             par.diff = int(x[3])
-        #-leap=leaper y/n
+        # -leap=leaper y/n
         if bool(re.fullmatch(r"-leap", x)):
             par.leap = 1
     return par
 
-print(read_cmd())
 
 def dimin(x, a, b):
     """
-    Implements a D2 style diminishing returns formula which takes input x and
-    modifies it with parameters a,b.
+    Implement a D2 style diminishing returns formula.
+
+    Takes input x and modifies it with parameters a and b.
         f(x)=int(a*x/(x+b))
     """
     return int(a*x/(b+x))
 
-#"Mod": (ResType, ResBonus)
-mod_dic = {"": (0, 0), "SS": (0, 50), "MB": (1, 20), 
-           "CE": (2, 75), "FE": (3, 75), "LE": (4, 75)}
 
 class ResistVector:
+    """Container for all resistances of a monster."""
+
     def __init__(self, resistances):
         """
-        Initializes a ResistVector as a list of 6 resistances in the order:
+        Initialize the ResistVector as a list of 6 resistances.
+
+        Order of resistances:
             Physical, Magic, Cold, Fire, Lightning, Poison
         """
         self.res = resistances
+
     def lower_cap(self):
+        """Apply the lower cap of -100 to the ResistVector (IN PLACE)."""
+        self.res = [max(-100, x) for x in self.res]
+
+    def conviction(self, slvl=0):
         """
-        IN PLACE
-        Applies the lower cap of -100 to the ResistVector.        
-        """
-        self.res=[max(-100, x) for x in self.res]
-    def conviction(self, slvl = 0):
-        """
-        IN PLACE
-        Applies slvl Conviction to the ResistVector, for reference:
-            Fire/Cold/Lightning Resist -% = min(25+5*slvl,150)                
+        Apply slvl Conviction to the ResistVector (IN PLACE).
+
+        Provides:
+            -min(25+5*slvl, 150) %
+        to cold, fire and lightning resistance.
         """
         malus = min(25+5*slvl, 150) if slvl else 0
         for i in range(2, 5):
-            self.res[i] = self.res[i]-(malus if self.res[i] < 100 else malus//5)
+            real_malus = malus if self.res[i] < 100 else malus//5
+            self.res[i] = self.res[i]-real_malus
         self.lower_cap()
+
     def lower_resist(self, slvl):
         """
-        IN PLACE
-        Applies slvl Lower Resist to the ResistVector, for reference:
-            Fire/Cold/Light/Poison Resist -% = min(25+45*dimin(slvl,110,6)/100), 70)                
-        with dimin(slvl,110,6)=110*slvl/(6+slvl)
+        Apply slvl Lower Resist to the ResistVector (IN PLACE).
+
+        Provides:
+           -min(25+45*dimin(slvl, 110, 6)/100), 70) %
+        to all resistances except magic and physcial, with:
+            dimin(slvl, 110, 6)=110*slvl/(6+slvl)
         """
         malus = min(25+(45*dimin(slvl, 110, 6))//100, 70) if slvl else 0
         for i in range(2, 6):
-            self.res[i] = self.res[i]-(malus if self.res[i] < 100 else malus//5)
+            real_malus = malus if self.res[i] < 100 else malus//5
+            self.res[i] = self.res[i]-real_malus
         self.lower_cap()
+
     def enemy_resist(self, malus):
-        '''
-        IN PLACE
-        Applies the malus vector consisting of -EPR, -EMR, -ECR, -EFR, -ELR
-        and -EPR. to the ResistVector.
-        '''
-        self.res=[x-malus[i] if x < 100 else x for (i, x) in enumerate(self.res)]
+        """
+        Apply -% enemy resist to the ResistVector (IN PLACE).
+
+        malus vector consists of -EPhR, -EMR, -ECR, -EFR, -ELR and -EPR.
+        """
+        self.res = [x-m if x < 100 else x for (x, m) in zip(self.res, malus)]
         self.lower_cap()
+
     def cold_mastery(self, slvl):
-        '''
-        IN PLACE
-        Applies slvl Cold Mastery to the Resist Vector, for reference:
-            Cold Resist -% = 15+5*slvl
-        '''
+        """
+        Apply slvl Cold Mastery to the Resist Vector (IN PLACE).
+
+        Provides:
+            -(15+5*slvl) %
+        to enemy cold resist.
+        """
         malus = 15+5*slvl if slvl else 0
         if self.res[2] < 100:
             self.res[2] -= malus
         self.lower_cap()
+
     def generic_bonus(self, res_type, bonus):
-        '''
-        IN PLACE
-        Applies bonus to resistance of type res_type if this does not result
-        in a third immunity or increases one of two or more immunities.
-        '''
+        """
+        Apply bonus to resistance (IN PLACE).
+
+        Adds bonus to type resistance of index res_type if this does not
+        result in a third immunity or increase one of two or more immunities.
+        """
         if self.res[res_type] < 100-bonus:
             self.res[res_type] += bonus
         elif 2 <= sum(1 for x in self.res if 100 <= x):
             pass
         else:
             self.res[res_type] += bonus
+
     def apply_mod(self, mod):
         """
-        IN PLACE
-        Applies the resistance bonus of "mod" to the Resist Vector. Key:
+        Apply the mod's bonuses to the ResistVector (IN PLACE).
+
+        Key:
         SS - Stone Skin             +50 to phys. resistance
         MB - Mana Burn              +20 to magic resistance
         CE - Cold Enchangted        +75 to cold resistance
@@ -169,9 +199,9 @@ class ResistVector:
                                     if respective resistance is less than 75
         MR - Magic Resistant        +40 to cold, fire and lightning resistance
                                     if respective resistance is less than 100
-                                    and this does not result in a third immunity
+                                    and it does not result in a third immunity
                                     or increase one of two immunities
-           - else                   +0 to all       
+           - else                   +0 to all
         """
         if mod == "MR":
             for i in range(2, 5):
@@ -185,24 +215,25 @@ class ResistVector:
             self.generic_bonus(mod_dic[mod][0], mod_dic[mod][1])
 
 
-
 def resist_list(resistances, par):
     """
-    Magic Resistant may not spawn in Normal difficulty and Multiple Shot only
-    spawns for monsters that have rtype set to 1.
-    
-    Returns
-    -------
-    One Counter object for each resistance. Each counter has the possible
-    resistances as keys and the number of their occurence in all the possible
-    modifier permutations as values.
+    Return Counter with possible values/multiplicities for each resistance.
+
+    Each Counter object has the possible resistance values as keys and the
+    amount of their occurences in all modifier permutations as values.
+
+    Modifier list consists of all 13 modifiers minus up to three:
+        Magic Resistant         only in nightmare & hell
+        Multiple Shots          only on ranged monsters.
+        Lightning Enchanted     not on leapers
     """
-    mods = ["FE", "CE", "SS", "SH", "MB"]
-    mods.extend(["LE"]*(par.leap == 0)+[""]*(5+par.rtype)+["MR"]*(par.diff != 0))
-    l = []
-    def temp_func(res_vec,par,mod_vec):
+    mods = ["FE", "CE", "SS", "SH", "MB"].extend(["LE"]*(par.leap == 0))
+    mods.extend([""]*(5+par.rtype)+["MR"]*(par.diff != 0))
+    lst = []
+
+    def temp_func(res_vec, par, mod_vec):
         for j in range(0, par.diff+1):
-            if mod_vec[j]!="":
+            if mod_vec[j] != "":
                 res_vec.apply_mod(mod_vec[j])
         if par.conv:
             res_vec.conviction(par.conv)
@@ -213,37 +244,36 @@ def resist_list(resistances, par):
         if sum(par.er):
             res_vec.enemy_resist(par.er)
         return res_vec
-        
     for i in permutations(mods, par.diff+1):
         temp = ResistVector(resistances.res[:])
         temp = temp_func(temp, par, i)
-        l.append(temp.res)
-    r = tuple(zip(*l))
+        lst.append(temp.res)
+    r = tuple(zip(*lst))
     return [Counter(r[i]) for i in range(0, 6)]
 
-def percentage_list(resistances, par):
-    '''
-    Returns a list of possible resistances and their probability for the given
-    res_type.
 
-    Parameters
-    ----------
-    res_type: Resistance Type
-    '''
+def percentage_list(resistances, par):
+    """Return a list of possible resistances and their probability."""
     def percentages(dic):
         total = sum(dic.values())
-        return {key:"{:.2%}".format(dic[key]/total) for key in dic.keys()}
+        return {key: "{:.2%}".format(dic[key]/total) for key in dic.keys()}
     res_list = resist_list(resistances, par)
-    for i in range(0,6):
+    for i in range(0, 6):
         pairs = sorted(percentages(res_list[i]).items())
-        print("\n{}:".format(res_dic[i]))
+        print("\n{}:".format(res_d[i]))
         for a in pairs:
             print("{}: {}".format(a[0], a[1]))
 
-#Plague Bearer (Hell)
-#Resists = ResistVector([50, 100, 0, 0, 0, 75])
 
-Resists=ResistVector([int(x) for x in input("Enter Resistances: ").split(" ")])
+def main():
+    """Get resistances from user and output statistic and options."""
+    # Plague Bearer (Hell)
+    # Resists = ResistVector([50, 100, 0, 0, 0, 75])
+    prompt = "Enter Resistances: "
+    print(read_cmd())
+    Resists = ResistVector([int(x) for x in input(prompt).split(" ")])
+    percentage_list(Resists, read_cmd())
 
 
-percentage_list(Resists, read_cmd())
+if __name__ == "__main__":
+    main()
